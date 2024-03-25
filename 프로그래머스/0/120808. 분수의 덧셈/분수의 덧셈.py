7428481a9078f865110faf2b8979633a8972d0de
jeonghani <81557284+jeonghani@users.noreply.github.com>
2024-03-25 21:21:21 +0900

    Delete 프로그래머스/0 directory

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def solution(numer1, denom1, numer2, denom2):
-    a = denom1*denom2
-    b = numer1*denom2 + numer2*denom1
-    c = math.gcd(a, b)
-    answer = [b/c, a/c]
-    return answer
--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.py
+++
@@ -1,7 +0,0 @@
-def solution(n):
-    if n%7==0 :
-        return n//7
-    elif n<7 :
-        return 1
-    else :
-        return n//7+1

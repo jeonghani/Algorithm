53663d0b9a121721aab9c169346f25159bec5610
jeonghani <81557284+jeonghani@users.noreply.github.com>
2025-02-19 07:41:12 +0900

    [level 1] Title: 실패율, Time: 17.48 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,19 +1,26 @@
 def solution(N, stages):
+    answer = []
     player = [0] * (N+1)
     players = len(stages)
-    failure = []
+    print(players)
+    
     for i in stages:
         if i<=N:
             player[i] += 1
+    
+    failure = []
+    
     for i in range(1, N+1):
         if players == 0:
             failure_rate = 0
         else:
             failure_rate = player[i]/players
         failure.append((i, failure_rate))
-        players-=player[i]
+        players -= player[i]
+    
     failure.sort(key=lambda x:x[1], reverse=True)
-    answer = []
+    
     for i in failure:
         answer.append(int(i[0]))
+    
     return answer

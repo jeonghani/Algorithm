e108c225f4d89a7285bb6c1c922e6fd2ae6ddeed
jeonghani <81557284+jeonghani@users.noreply.github.com>
2025-02-19 11:05:59 +0900

    [level 1] Title: 둘만의 암호, Time: 0.05 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,11 +1,10 @@
 def solution(s, skip, index):
     answer = ''
     alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
-    valid_chars = [ch for ch in alphabet if ch not in skip]
+    valid = [ch for ch in alphabet if ch not in skip]
     
     for i in s:
-        char_index = valid_chars.index(i)
-        new = valid_chars[(char_index+index)%len(valid_chars)]
-        answer += new
-    
+        char_index = valid.index(i)
+        change_index = (char_index+index) % len(valid)
+        answer += valid[change_index]
     return answer

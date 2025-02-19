ed554c9832353a23979afca5d21c5d3149161551
jeonghani <81557284+jeonghani@users.noreply.github.com>
2025-02-19 22:17:04 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 0.92 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -0,0 +1,21 @@
+def solution(mats, park):
+    
+    n, m = len(park), len(park[0])
+    dp = [[0]*m for _ in range(n)]
+    max_square = 0
+    
+    for i in range(n):
+        for j in range(m):
+            if park[i][j] == "-1":
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                max_square = max(max_square, dp[i][j])
+    
+    mats.sort(reverse=True)
+    for size in mats:
+        if size <= max_square:
+            return size
+    
+    return -1

f265532856353aa7dd7650dac91c00f5fb730f6f
jeonghani <81557284+jeonghani@users.noreply.github.com>
2025-06-04 19:55:02 +0900

    [level 1] Title: 숫자 짝꿍, Time: 79.36 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/README.md
+++ 프로그래머스/1/131128. 숫자 짝꿍/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 짝꿍 - 131128 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131128) 
 
 ### 성능 요약
 
-메모리: 144 MB, 시간: 90.30 ms
+메모리: 144 MB, 시간: 79.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 04일 17:32:56
+2025년 06월 04일 19:54:58
 
 ### 문제 설명
 
--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -12,6 +12,24 @@ class Solution {
         for (char c : Y.toCharArray()) {
             yCount[c - '0']++;
         }
+        
+//         String answer = "";
+
+//         // 9부터 0까지 공통 숫자를 큰 숫자부터 이어붙이기
+//         for (int i = 9; i >= 0; i--) {
+//             int common = Math.min(xCount[i], yCount[i]);
+//             for (int j = 0; j < common; j++) {
+//                 answer += i; // 문자열 + 연산
+//             }
+//         }
+
+//         // 공통 숫자가 없으면 -1
+//         if (answer.equals("")) return "-1";
+
+//         // 전부 0이면 "0"
+//         if (answer.charAt(0) == '0') return "0";
+
+//         return answer;
 
         StringBuilder sb = new StringBuilder();
 
@@ -26,7 +44,7 @@ class Solution {
         // 아무 숫자도 없다면
         if (sb.length() == 0) return "-1";
 
-        // 0으로만 구성되어 있다면 (예: "000")
+        // 0으로만 구성되어 있다면 "0000"
         if (sb.charAt(0) == '0') return "0";
 
         return sb.toString();

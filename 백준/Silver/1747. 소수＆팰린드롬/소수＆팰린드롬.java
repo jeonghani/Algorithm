2622d57cf0785eb49b61dabb94780b2325131d61
jeonghani <81557284+jeonghani@users.noreply.github.com>
2025-07-28 21:29:11 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 160 ms, Memory: 19556 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -1,33 +1,37 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        for (int i = N; ; i++) {
-            if (isPalindrome(i) && isPrime(i)) {
+        int MAX = 1100000; // 1,000,000보다 조금 더 큰 범위
+        boolean[] isNotPrime = new boolean[MAX + 1];
+        isNotPrime[0] = isNotPrime[1] = true;
+
+        // 에라토스테네스의 체
+        for (int i = 2; i * i <= MAX; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j <= MAX; j += i) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+
+        for (int i = N; i <= MAX; i++) {
+            if (!isNotPrime[i] && isPalindrome(i)) {
                 System.out.println(i);
                 break;
             }
         }
     }
 
-    // 팰린드롬 판정
     static boolean isPalindrome(int n) {
         String s = Integer.toString(n);
         int len = s.length();
         for (int i = 0; i < len / 2; i++) {
             if (s.charAt(i) != s.charAt(len - i - 1)) return false;
-        }
-        return true;
-    }
-
-    // 소수 판정
-    static boolean isPrime(int n) {
-        if (n < 2) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) return false;
         }
         return true;
     }

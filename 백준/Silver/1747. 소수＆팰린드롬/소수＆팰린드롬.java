0b1c2889c6c437500f76dac26007f3dc0cdbc4fe
jeonghani <81557284+jeonghani@users.noreply.github.com>
2025-07-28 21:28:16 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 220 ms, Memory: 40000 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; ; i++) {
+            if (isPalindrome(i) && isPrime(i)) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+
+    // 팰린드롬 판정
+    static boolean isPalindrome(int n) {
+        String s = Integer.toString(n);
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) return false;
+        }
+        return true;
+    }
+
+    // 소수 판정
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
